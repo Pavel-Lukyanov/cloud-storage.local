1c686fe814588b11c57e72618e2fc44f9a717ac4
Лукьянов Павел <pahan-01.04.10@mail.ru>
2023-05-06 19:07:54 +0500

    add dump database

--- cloud_storage.sql
+++ cloud_storage.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 04 2023 г., 10:37
+-- Время создания: Май 06 2023 г., 17:07
 -- Версия сервера: 8.0.30
 -- Версия PHP: 8.1.9
 
@@ -30,10 +30,9 @@ SET time_zone = "+00:00";
 CREATE TABLE `files` (
   `id` int NOT NULL,
   `file_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Название файла',
-  `original_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Оригинальное название файла',
+  `original_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Оригинальное название файла',
   `user_id` int NOT NULL COMMENT 'id польователя которому принадлежит файл',
   `parent_folder_id` int DEFAULT NULL COMMENT 'id папки родительской папки',
-  `file_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Путь к файлу на сервере',
   `file_size` bigint NOT NULL COMMENT 'Размер файла',
   `file_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Тип файла',
   `file_created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата создания файла'
@@ -43,9 +42,8 @@ CREATE TABLE `files` (
 -- Дамп данных таблицы `files`
 --
 
-INSERT INTO `files` (`id`, `file_name`, `original_name`, `user_id`, `parent_folder_id`, `file_path`, `file_size`, `file_type`, `file_created_at`) VALUES
-(8, '1_1683181793_Скриншот 04-05-2023 095146.jpg', 'Скриншот 04-05-2023 095146.jpg', 1, 1, '/files/1/', 60185, 'image/jpeg', '2023-05-04 06:29:53'),
-(9, '1_1683182315_Скриншот 04-05-2023 095146.jpg', 'Скриншот 04-05-2023 095146.jpg', 1, 8, '/files/1/новая папка/Еще одна новая папка вложенная/', 60185, 'image/jpeg', '2023-05-04 06:38:35');
+INSERT INTO `files` (`id`, `file_name`, `original_name`, `user_id`, `parent_folder_id`, `file_size`, `file_type`, `file_created_at`) VALUES
+(22, '1_1683379351_screenshot.png', 'screenshot.png', 1, 3, 30049, 'image/png', '2023-05-06 09:41:54');
 
 -- --------------------------------------------------------
 
@@ -55,10 +53,9 @@ INSERT INTO `files` (`id`, `file_name`, `original_name`, `user_id`, `parent_fold
 
 CREATE TABLE `folders` (
   `id` int NOT NULL,
-  `folder_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT ' Название папки',
+  `folder_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT ' Название папки',
   `user_id` int NOT NULL COMMENT 'Пользователь создавший папку',
   `parent_folder_id` int DEFAULT NULL COMMENT 'Родительская папка',
-  `folder_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Путь к папке на сервере',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата создания папки',
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата обновления папки'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -67,12 +64,11 @@ CREATE TABLE `folders` (
 -- Дамп данных таблицы `folders`
 --
 
-INSERT INTO `folders` (`id`, `folder_name`, `user_id`, `parent_folder_id`, `folder_path`, `created_at`, `updated_at`) VALUES
-(1, '1', 1, NULL, '/files/1/', '2023-05-04 08:46:50', '2023-05-04 08:46:50'),
-(7, 'новая папка', 1, 1, '/files/1/новая папка/', '2023-05-04 08:51:39', '2023-05-04 08:51:39'),
-(8, 'Еще одна новая папка вложенная', 1, 7, '/files/1/новая папка/Еще одна новая папка вложенная/', '2023-05-04 08:51:59', '2023-05-04 08:51:59'),
-(9, 'Плюс Еще одна новая папка вложенная', 1, 7, '/files/1/новая папка/Плюс Еще одна новая папка вложенная/', '2023-05-04 08:52:28', '2023-05-04 08:52:28'),
-(10, 'test', 1, 8, '/files/1/новая папка/Еще одна новая папка вложенная/test/', '2023-05-04 10:35:37', '2023-05-04 10:35:37');
+INSERT INTO `folders` (`id`, `folder_name`, `user_id`, `parent_folder_id`, `created_at`, `updated_at`) VALUES
+(1, '1', 1, NULL, '2023-05-04 08:46:50', '2023-05-04 08:46:50'),
+(2, 'img', 1, 1, '2023-05-06 11:48:06', '2023-05-06 11:48:06'),
+(3, 'videos', 1, 1, '2023-05-06 11:48:45', '2023-05-06 11:48:45'),
+(4, 'Skillbox', 1, 1, '2023-05-06 17:07:03', '2023-05-06 17:07:03');
 
 -- --------------------------------------------------------
 
@@ -112,7 +108,7 @@ CREATE TABLE `user_tokens` (
 --
 
 INSERT INTO `user_tokens` (`id`, `user_id`, `token`, `expiration_time`) VALUES
-(1, 1, '44349737c76c27bded2033d4965ad3fe5f596d628d68a43216784f14bf702878', '2023-05-05 08:47:28');
+(1, 1, '44349737c76c27bded2033d4965ad3fe5f596d628d68a43216784f14bf702878', '2023-05-19 08:47:28');
 
 --
 -- Индексы сохранённых таблиц
@@ -155,13 +151,13 @@ ALTER TABLE `user_tokens`
 -- AUTO_INCREMENT для таблицы `files`
 --
 ALTER TABLE `files`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT для таблицы `folders`
 --
 ALTER TABLE `folders`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
